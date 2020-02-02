2f07c6a05b81c21a44a026d29a2e8e3b87a7f0e4
daat2 <ibi.olu@gmail.com>
2020-02-02 22:51:32 +0000

    remove pplay mode

--- js/main.js
+++ js/main.js
@@ -19,30 +19,36 @@ const start = document.querySelector('.start');
 const strict = document.querySelector('strict');
 
 start.addEventListener('click', (event) => {
-      $(".counter").text("00");
-        let number = generateRandomNumber()
-    game.playSequence.push(number)
-    play(game.playSequence)
-});
-function generateRandomNumber(){
-    return Math.floor(Math.random() * 4)
-}
-function derive(number){
-    switch(number){
-        case 0:
-            return [blue, boardSound[0], "blue-active"]
-        case 1:
-            return [green, boardSound[1], "green-active"]
-        case 2:
-            return [orange, boardSound[2], "orange-active"]
-        case 3:
-            return [yellow, boardSound[3], "yellow-active"]
-    }
-}
-function play(sequence){
-    sequence.forEach(function (number) {
-        let [button, sound, className] = derive(number)
-        button.classList.add(className)
-        new Audio(sound).play()
-    })
-}
+    $("counter"). text("");
+    strict =false;
+    error =false;
+    level =0;
+    level ++;
+
+//       $(".counter").text("00");
+//         let number = generateRandomNumber()
+//     game.playSequence.push(number)
+//     play(game.playSequence)
+// });
+// function generateRandomNumber(){
+//     return Math.floor(Math.random() * 4)
+// }
+// function derive(number){
+//     switch(number){
+//         case 0:
+//             return [blue, boardSound[0], "blue-active"]
+//         case 1:
+//             return [green, boardSound[1], "green-active"]
+//         case 2:
+//             return [orange, boardSound[2], "orange-active"]
+//         case 3:
+//             return [yellow, boardSound[3], "yellow-active"]
+//     }
+// }
+// function play(sequence){
+//     sequence.forEach(function (number) {
+//         let [button, sound, className] = derive(number)
+//         button.classList.add(className)
+//         new Audio(sound).play()
+//     })
+// }
