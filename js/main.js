48500e56e2f6e5fa5b85fdbd14404f5876292f44
daat2 <ibi.olu@gmail.com>
2020-02-12 13:28:57 +0000

    add listner

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const game = {
     playerSequence: [], //array containing the user selected button
     simonSequence: [], //array containing simon(computer) random button
     numLevels: 20,  //gave max level
-    turn : 0, // current turn
+   
 
 };
 const boardSound = [
@@ -17,69 +17,17 @@ const green = document.querySelector('#shapeY');
 const orange = document.querySelector('#shapeG');
 const yellow = document.querySelector('#shapeO');
 const start = document.querySelector('.start');
-const strict = document.querySelector('#strict');
+const strictButton = document.querySelector('#strict')
+const counter= document.querySelector('.counter');
 
 
 let id,color,level=0;
 
 let error =false;
+let turn =0; //to keep count of game 
+let mode =false;
+let win; // to indicate if game is being won 
 
-
-// function for button flash 
-
-
-//  1 Start Simon Sequence 
-
-start.addEventListener('click', (event) => {
-    $(".counter").text("01") 
-  const newLocal = level++;
-  simonSequence=[];
-  playerSequence =[];
-    
-simonSequence ();
-})
-
-
-//Strict mode listener
-.mode addEventListener ('click' , (event) => {
-    if (strictButton.checked  == true) {
-        strict =true;
-    }else {
-           strict =false; 
-                   
-        }
-    
-
-    });
-
-
-
-
-
-
-//     let number = generateRandomNumber()
-//     game.simonSequence.push(number)
-//     play(game.simonSequence)
-// });
-// function generateRandomNumber(){
-//     return Math.floor(Math.random() * 4)
-//     function derive(number){
-//     switch(number){
-//         case 0:
-//             return [blue, boardSound[0], "blue-active"]
-//         case 1:
-//             return [green, boardSound[1], "green-active"]
-//         case 2:
-//             return [orange, boardSound[2], "orange-active"]
-//         case 3:
-//             return [yellow, boardSound[3], "yellow-active"]
-//     }
-// }
-// function play(sequence){
-//     sequence.forEach(function (number) {
-//         let [button, sound, className] = derive(number)
-//         button.classList.add(className)
-//         new Audio(sound).play()
-//     })
-// }
-// };
+document.getElementById("strict").addEventListener("click", function(){
+ console.log("checked")
+});
